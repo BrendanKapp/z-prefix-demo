e401ae7d3f819c29fadb36810e7cad87db1b7bf3
Brendan Kapp <brendankapp@protonmail.com>
2025-01-17 12:58:05 -0700

    Add add-item

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const ItemDetail = () => {
   const [item, setItem] = useState(null);
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get(`/api/items/${id}`)
       .then(response => setItem(response.data))
       .catch(error => console.log(error));
   }, [id]);
+
+  const back = (e) => {
+    navigate("/")
+  };
 
   if (!item) return <div>Loading...</div>;
 
