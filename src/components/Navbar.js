1cb36cf1ba5cc046417f325bad9631c12336c097
Brendan Kapp <brendankapp@protonmail.com>
2025-01-17 13:48:10 -0700

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,58 +1,29 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-
-// const Navbar = () => {
-
-//   const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem('authToken') !== null);
-
-//   const handleLogout = () => {
-//     localStorage.removeItem('authToken');
-//     setIsAuthenticated(false);
-//   };
-
-//   return (
-//     <nav>
-//       <ul>
-//         <li><Link to="/">All Items</Link></li>
-//         {isAuthenticated && <li><Link to="/add-item">Add Item</Link></li>}
-//         {isAuthenticated && <li><Link to="/inventory">Inventory</Link></li>}
-//         {isAuthenticated ? (
-//           <li><button onClick={handleLogout}>Logout</button></li>
-//         ) : (
-//           <>
-//             <li><Link to="/login">Login</Link></li>
-//             <li><Link to="/register">Register</Link></li>
-//           </>
-//         )}
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from './auth/AuthProvider.js';
+import './Navbar.css';
 
 const NavBar = () => {
   const { isAuthenticated, logout } = useAuth();
 
   return (
-    <ul>
-      {isAuthenticated ? (
-        <div>
-          <li><button onClick={logout}>Logout</button></li>
-          <li><Link to="/inventory">Inventory</Link></li>
-        </div>
-      ) : (
-        <>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
-        </>
-      )}
-      <li><Link to="/">All Items</Link></li>
-    </ul>
+    <nav className="navbar">
+      <ul className="navbar-links">
+        <li><Link to="/" className="navbar-link">All Items</Link></li>
+
+        {isAuthenticated ? (
+          <>
+            <li><Link to="/inventory" className="navbar-link">Inventory</Link></li>
+            <li><Link onClick={logout} className="navbar-button">Logout</Link></li>
+          </>
+        ) : (
+          <>
+            <li><Link to="/login" className="navbar-button">Login</Link></li>
+            <li><Link to="/register" className="navbar-button">Register</Link></li>
+          </>
+        )}
+      </ul>
+    </nav>
   );
 };
 
