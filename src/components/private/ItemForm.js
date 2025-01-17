8ec323197ce2cb5bbb4b92363ed5fd4674d0572a
Brendan Kapp <brendankapp@protonmail.com>
2025-01-17 13:48:41 -0700

    Style ItemForm

--- src/components/private/ItemForm.js
+++ src/components/private/ItemForm.js
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../auth/AuthProvider.js';
+import './ItemForm.css';
 
 const ItemForm = () => {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState('');
   const navigate = useNavigate();
-  
+
   const { isAuthenticated, logout } = useAuth();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const itemData = { name, description, quantity };
 
-    // Only handle form submission if the user is authenticated
     if (!isAuthenticated) {
       console.log("No token found or not authenticated");
       logout();
@@ -28,40 +28,48 @@ const ItemForm = () => {
       Authorization: `Bearer ${authToken}`,
     };
 
-    // Sending POST request to add a new item
     axios.post('/api/items', itemData, { headers })
-      .then(() => navigate('/'))
+      .then(() => navigate('/inventory'))
       .catch(error => console.log(error));
   };
 
   return (
-    <div>
-      <h1>Add Item</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name:</label>
+    <div className="item-form-container">
+      <h1 className="form-title">Add Item</h1>
+      <form onSubmit={handleSubmit} className="item-form">
+        <div className="input-group">
+          <label htmlFor="name" className="input-label">Name:</label>
           <input
             type="text"
+            id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            className="input-field"
+            required
           />
         </div>
-        <div>
-          <label>Description:</label>
+        <div className="input-group">
+          <label htmlFor="description" className="input-label">Description:</label>
           <textarea
+            id="description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
+            className="textarea-field"
+            required
           />
         </div>
-        <div>
-          <label>Quantity:</label>
+        <div className="input-group">
+          <label htmlFor="quantity" className="input-label">Quantity:</label>
           <input
             type="number"
+            id="quantity"
             value={quantity}
             onChange={(e) => setQuantity(e.target.value)}
+            className="input-field"
+            required
           />
         </div>
-        <button type="submit">Add Item</button>
+        <button type="submit" className="submit-button">Add Item</button>
       </form>
     </div>
   );
