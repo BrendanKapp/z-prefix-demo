ee7bc259e24e185d8ba6812419770182f67705d7
Brendan Kapp <brendankapp@protonmail.com>
2025-01-17 13:48:23 -0700

    Style RegisterForm

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './RegisterForm.css';
 
 const RegisterForm = () => {
   const [email, setEmail] = useState('');
@@ -16,30 +17,33 @@ const RegisterForm = () => {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
+    <div className="register-container">
+      <h1 className="register-title">Create an Account</h1>
+      <form onSubmit={handleSubmit} className="register-form">
+        <div className="input-group">
+          <label htmlFor="email">Email:</label>
           <input
             type="email"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
-        <div>
-          <label>Password:</label>
+        <div className="input-group">
+          <label htmlFor="password">Password:</label>
           <input
             type="password"
+            id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </div>
-        <button type="submit">Register</button>
+        <button type="submit" className="register-button">Register</button>
       </form>
     </div>
   );
 };
 
-export default RegisterForm;;
-;
+export default RegisterForm;
