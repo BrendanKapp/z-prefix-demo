9de30a21b9101509d1f3659708380f6340817778
Brendan Kapp <brendankapp@protonmail.com>
2025-01-17 13:47:39 -0700

    Style ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './ItemList.css';
 
 const ItemList = () => {
   const [items, setItems] = useState([]);
@@ -12,13 +13,16 @@ const ItemList = () => {
   }, []);
 
   return (
-    <div>
-      <h1>All Items</h1>
-      <ul>
+    <div className="item-list-container">
+      <h1 className="item-list-title">All Items</h1>
+      <ul className="item-list">
         {items.map(item => (
-          <li key={item.id}>
-            <Link to={`/items/${item.id}`}>
-            {item.name} - {item.description.length > 100 ? item.description.slice(0, 100) + '...' : item.description}
+          <li key={item.id} className="item-card">
+            <Link to={`/items/${item.id}`} className="item-link">
+              <h2 className="item-name">{item.name}</h2>
+              <p className="item-description">
+                {item.description.length > 100 ? item.description.slice(0, 100) + '...' : item.description}
+              </p>
             </Link>
           </li>
         ))}
@@ -28,4 +32,3 @@ const ItemList = () => {
 };
 
 export default ItemList;
-
