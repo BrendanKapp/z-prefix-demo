050148b076fbb889aeeffd3900bdf72d67b6f11c
Brendan Kapp <brendankapp@protonmail.com>
2025-01-17 09:38:01 -0700

    Add state to logout so no refresh is required

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const isAuthenticated = localStorage.getItem('authToken');
+
+  const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem('authToken') !== null);
+
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    setIsAuthenticated(false); // Update state to trigger re-render
+  };
 
   return (
     <nav>
@@ -11,7 +17,7 @@ const Navbar = () => {
         {isAuthenticated && <li><Link to="/add-item">Add Item</Link></li>}
         {isAuthenticated && <li><Link to="/inventory">Inventory</Link></li>}
         {isAuthenticated ? (
-          <li><button onClick={() => localStorage.removeItem('authToken')}>Logout</button></li>
+          <li><button onClick={handleLogout}>Logout</button></li>
         ) : (
           <>
             <li><Link to="/login">Login</Link></li>
