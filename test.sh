32cab5ae2b7fa74e53f3c2b5f3b50a47ae2bc535
Brendan Kapp <brendankapp@protonmail.com>
2025-01-20 09:14:54 -0700

    Remove test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-function title() {
-    # Set terminal tab title. Usage: title "new tab name"
-    prefix=${PS1%%\\a*}                  # Everything before: \a
-    search=${prefix##*;}                 # Eeverything after: ;
-    esearch="${search//\\/\\\\}"         # Change \ to \\ in old title
-    PS1="${PS1/$esearch/$@}"             # Search and replace old with new
-}
-
